3c79738050c0477608d69c059108f34eda26d147
Michael <michael.sidon@gmail.com>
2014-02-26 18:47:02 +0000

    First commit with bit of refactoring

--- directory_pro.rb
+++ directory_pro.rb
@@ -1,101 +1,133 @@
+@students = [] # an empty array accessible to all methods
+
 #let's put all students into an array
 def input_students
-	print "Please enter the name of the students \n"
-	print "To finish, just hit return twice at the name \n"
-	#create an empty array
-	students = []
-	#get the first name
-	name = gets.chomp
-	puts "Cohort please?"
-	cohort = gets.sub("\n"," ")
-puts "Are you sure brother? y/n"
-		sure = gets.chomp
-		if sure == "n"
-			puts "Cohort please?"
-			cohort = gets.chomp
-		elsif cohort.empty?
-		cohort = "unknown"
-	end
-	puts "Hobby?"
-	hobby = gets.chomp
-	puts "Country?"
-	country = gets.chomp
-	#while the name is not empty, repeat this code
-	while !name.empty? do
-		#add the student hash to the array
-		students << {:name => name, :cohort => cohort, :hobby => hobby, :country => country}
-		if students.length > 1
-		print "Now we have #{students.length} students. \n"
-		else print "Now we have #{students.length} student. \n"
-		end
-		name = gets.chomp
-	
-		
-				puts "Cohort please?"
-				cohort = gets.chomp
-				puts "Are you sure brother? y/n"
-				sure = gets.chomp
-			if sure == "n"
-				puts "Cohort please?"
-				cohort = gets.chomp
-			elsif cohort.empty?
-				cohort = "unknown"
-			end
-			puts "Hobby?"
-			hobby = gets.chomp
-			puts "Country?"
-			country = gets.chomp
-		
-	end
-		
-		
-		
-	
-	#return the array of students
-	students
-
+  print "Please enter the name of the students \n"
+  print "To finish, just hit return twice at the name \n"
+  #create an empty array
+  @students = []
+  #get the first name
+  name = gets.chomp
+  puts "Cohort please?"
+  cohort = gets.sub("\n"," ")
+  puts "Are you sure? y/n"
+  sure = gets.chomp
+  if sure == "n"
+    puts "Cohort please?"
+	  cohort = gets.chomp
+  elsif cohort.empty?
+    cohort = "unknown"
+  end
+  puts "Hobby?"
+  hobby = gets.chomp
+  puts "Country?"
+  country = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+  #add the student hash to the array
+    @students << {:name => name, :cohort => cohort, :hobby => hobby, :country => country}
+	  if @students.length > 1
+	    print "Now we have #{@students.length} students. \n"
+	  else print "Now we have #{@students.length} student. \n"
+	  end
+	  name = gets.chomp
+	break if name.empty?
+	  puts "Cohort please?"
+	  cohort = gets.chomp
+	  puts "Are you sure? y/n"
+	  sure = gets.chomp
+	  if sure == "n"
+	    puts "Cohort please?"
+	    cohort = gets.chomp
+	  elsif cohort.empty?
+	    cohort = "unknown"
+	  end
+	  puts "Hobby?"
+	  hobby = gets.chomp
+	  puts "Country?"
+	  country = gets.chomp
+  end	
+  #return the array of students
+  @students
 end
+
+
+def interactive_menu
+	@students = []
+  loop do
+    #1. print the menu and ask the user what to do
+    print_menu
+    #2. read the input and save it into a variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    process(selection)
+  end
+end 
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #9 because we'll be adding more items
+end
+
+def show_students
+	print_header
+	print_student_bycohort(@students)
+	print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+  	@students = input_students
+  when "2"
+  	show_students
+  when "9"
+  	exit #this will cause the program to terminate
+  else
+  	puts "I don't know what you meant, try again"
+  end
+end
+
+
 
 def print_header
-	puts "The students of my cohort at Makers Academy".center(100)
-	puts "------------- ".center(100)
+  puts "The students of my cohort at Makers Academy".center(100)
+  puts "------------- ".center(100)
 end
 
-def print_students(students)
-	if students.length > 0
-	i = 0
-	while i < students.length
-		student = students[i]
-	  	puts "#{student[:name]}, #{student[:hobby]}, #{student[:country]}, #{student[:cohort]}".center(100) 
-	  	i += 1
-	end
-	else puts "You have no students!"
-end
+def print_students(student)
+  if student.length > 0
+	  i = 0
+	  while i < student.length
+	    stud = student[i]
+	    puts "#{stud[:name]}, #{stud[:hobby]}, #{stud[:country]}, #{stud[:cohort]}".center(100) 
+	    i += 1
+	  end
+  else puts "You have no students!"
+  end
 end
 
 
-def print_student_bycohort(students)
-	if students.length > 0
-	puts "------------------".center(100)
-	puts "The students sorted by cohort".center(100)
-	sorted = students.sort {|x,y| x[:cohort] <=> y[:cohort]}
-	sorted.each_with_index do |student, index|
-		puts "#{index +1}. #{student[:name]}, #{student[:hobby]}, #{student[:country]}, #{student[:cohort]}".center(100)
-	end
-end
+def print_student_bycohort(student)
+  if student.length > 0
+	  #puts "------------------".center(100)
+	  sorted = student.sort {|x,y| x[:cohort] <=> y[:cohort]}
+	  sorted.each_with_index do |stud, index|
+	    puts "#{index +1}. #{stud[:name]}, #{stud[:hobby]}, #{stud[:country]}, #{stud[:cohort]}".center(100)
+	  end
+  end
 end
 
 
 def print_footer(names)
-	print "Overall, we have #{names.length} great students. \n"
+  print "Overall, we have #{names.length} great students. \n"
 end
 
 #nothing happens until we call the methods
-students = input_students
-print_header
-print_students(students)
-print_student_bycohort(students)
-print_footer(students)
+interactive_menu
+
+
 
 	
 
